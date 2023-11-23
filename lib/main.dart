e511dcd37130a72e68028a269a5dd7eb570c815b
Amal K <amalk@Amals-MacBook-Air.local>
2023-11-23 16:39:39 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                       width: 20,
                     ),
                     Text(
-                      'ASSET LOAN APPLICATION FORM',
+                      '- ASSET LOAN APPLICATION FORM',
                       style: GoogleFonts.poppins(
                           color: Color(0xFFff7300),
                           fontSize: 21,
